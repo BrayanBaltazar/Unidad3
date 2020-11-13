16d7ee36e0362e0645fbd71fcff1afabf3b7c79a
BrayanBaltazar <71296780+BrayanBaltazar@users.noreply.github.com>
2020-11-13 04:11:50 -0800

    avance

---
+++ Cine/Controllers/ProximamenteController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Cine.Controllers
+{
+    public class ProximamenteController : Controller
+    {
+        // GET: Proximamente
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
